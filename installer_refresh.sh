96f98bcc3d6e4398cb8c11c7bb42a43e5a5c96f9
Jiri Stransky <jistr@redhat.com>
2014-10-15 14:20:26 +0200

    Installer refresh script

---
+++ installer_refresh.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+STAYPUFT_INSTALLER_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
+
+FOREMAN_INSTALLER_DIR=${FOREMAN_INSTALLER_DIR:-$STAYPUFT_INSTALLER_DIR/../foreman-installer_git}
+
+if [ ! -d "$FOREMAN_INSTALLER_DIR" ]; then
+    echo "FOREMAN_INSTALLER_DIR doesn't exist: '$FOREMAN_INSTALLER_DIR'"
+fi
+
+cp -r "$STAYPUFT_INSTALLER_DIR/modules" "$FOREMAN_INSTALLER_DIR"
+cp -r "$STAYPUFT_INSTALLER_DIR/hooks" "$FOREMAN_INSTALLER_DIR"
+
+if [[ "$STAYPUFT_INSTALLER_BIN" = 'true' ]]; then
+    cp "$STAYPUFT_INSTALLER_DIR/bin/staypuft-installer" /usr/sbin
+    chmod a+x /usr/sbin/staypuft-installer
+    cp "$STAYPUFT_INSTALLER_DIR/bin/staypuft-client-installer" /usr/sbin
+    chmod a+x /usr/sbin/staypuft-client-installer
+fi
+
+if [[ "$STAYPUFT_INSTALLER_CONFIG" = 'true' ]]; then
+    cp "$STAYPUFT_INSTALLER_DIR/config/staypuft-installer.answers.yaml" /etc/foreman
+    cp "$STAYPUFT_INSTALLER_DIR/config/staypuft-installer.yaml" /etc/foreman
+    cp "$STAYPUFT_INSTALLER_DIR/config/staypuft-client-installer.yaml" /etc/foreman
+fi
+
+if [[ "$STAYPUFT_INSTALLER_MODULES" = 'true' ]]; then
+    rm -rf /etc/puppet/environments/production/modules/foreman
+    cp -r "$FOREMAN_INSTALLER_DIR/modules/foreman" /etc/puppet/environments/production/modules/foreman
+fi
